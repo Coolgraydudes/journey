1322abea5f4598e4ee87de035b42409d28806510
Coolgraydudes <144977851+Coolgraydudes@users.noreply.github.com>
2025-11-10 13:03:39 +0700

    Add files via upload

---
+++ src/Component/Prizing.jsx
@@ -0,0 +1,24 @@
+
+
+
+export default function Prizing() {
+      return (
+        <>
+            <section className="h-[70vh] relative flex justify-center items-center text-white bg-nig">
+                <div>
+                    <div className="flex md:flex-row flex-col gap-[200px] items-end ">
+                        <p className="w-[380px] text-[20px] mb-5 text-2xl">Explore our competitive pricing
+                            packages designed to suit your web
+                            design needs and budget, all crafted
+                            by Izzamnuddin
+                        </p>
+
+                        <button className="border-2 border-prim text-prim px-6 py-2 rounded-lg w-[170px] h-[50px]">Get the details</button>
+                    </div>
+
+                    <h1 className="text-8xl md:text-9xl font-poppins font-extrabold md:absolute text-center md:-bottom-12 md:left-48">Izzam Prizing</h1>
+                </div>
+            </section>
+        </>
+      )
+}
